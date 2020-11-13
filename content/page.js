6f2c256cce6db1147f2f7f364a14c2562c35df73
Nishimura_Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2020-11-13 09:05:15 -0700

    Very basic login example.

--- content/page.js
+++ content/page.js
@@ -1,17 +1,24 @@
 const utilities = require('../utilities.js');
 
-module.exports.title = function () {
-  return 'Title';
-};
+module.exports = {
+  title () {
+    return 'Title';
+  },
 
-module.exports.body = async function ({ req, session }) {
-  let data = await utilities.getPostData(req);
+  async body ({ req, session }) {
+    let data = await utilities.getPostData(req);
 
-  if (data && data.username === 'test' && data.password === 'test') {
-    session.loggedIn = true;
-  } else {
-    session.loggedIn = false;
+    // Example login
+    if (data && data.username === 'test' && data.password === 'test') {
+      session.loggedIn = data.username;
+    } else {
+      session.loggedIn = false;
+    }
+
+    return renderTemplate(session.loggedIn ? 'content/loggedin.fragment.html' : 'content/loggedout.fragment.html');  
+  },
+
+  getUserName ({ session }) {
+    return session.loggedIn;
   }
-
-  return renderTemplate(session.loggedIn ? 'content/loggedin.fragment.html' : 'content/loggedout.fragment.html');
 };
--- settings.js
+++ settings.js
@@ -9,5 +9,5 @@ module.exports = {
       httpOnly: true,
       secure: false,
     },
-  }
+  },
 };
